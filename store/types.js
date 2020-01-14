c26df14ce924e17d678844fe077adf9752572fb9
josemak25 <amakirij@gmail.com>
2020-01-14 15:06:58 +0100

    add app store types

---
+++ store/types.js
@@ -0,0 +1,8 @@
+// TODOS TYPES
+export const FETCH_TODOS_STARTED = "FETCH_TODOS_STARTED";
+export const FETCH_TODOS_SUCCESS = "FETCH_TODOS_SUCCESS";
+export const FETCH_TODOS_ERROR = "FETCH_BOOKS_ERROR";
+
+// APP API_BASE_URI
+export const API_BASE_URI =
+  "http://www.json-generator.com/api/json/get/cfIYghwKgi?indent=2";
