e486872f8ba6c5238bb8f271d8cad47bba001a6c
josemak25 <amakirij@gmail.com>
2020-01-14 15:15:11 +0100

    add todo list component

---
+++ screens/home/TodoList.js
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect } from "react";
+import { View, FlatList } from "react-native";
+import { StoreContext } from "../../store";
+import AppLoading from "../../components/Loading";
+import getTodos from "../../store/actions";
+import Todo from "../../components/Todo";
+
+export default function TodoList() {
+  const [store, dispatch] = useContext(StoreContext);
+
+  useEffect(() => {
+    getTodos()(dispatch);
+  }, []);
+
+  return !store.isLoading ? (
+    <View style={styles.todoList}>
+      <FlatList
+        data={store.todos}
+        renderItem={({ item }) => <Todo todo={item} />}
+        keyExtractor={todo => todo.id}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  ) : (
+    <AppLoading />
+  );
+}
