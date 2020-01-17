a4e4bdb2e95433ca7e9a66044422af02ac85e2ae
josemak25 <amakirij@gmail.com>
2020-01-17 12:44:46 +0100

    add user online and delete icons

--- components/Todo/index.js
+++ components/Todo/index.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Text, View, Image } from "react-native";
+import DeleteButton from "../../assets/DeleteButton";
+import Button from "../Button";
+
 import styles from "./styles";
 
 export default function Todo({ todo }) {
-  console.log(todo);
+  const { completed, createdAt, name, picture, id } = todo;
 
-  const { completed, createdAt, name, picture } = todo;
+  const deleteContact = id => {
+    console.tron.log({ id });
+  };
 
   return (
     <View style={styles.container}>
+      <View style={styles.online} />
       <Image source={{ uri: picture }} style={styles.picture} />
       <View style={styles.details}>
         <Text style={styles.todoText}>{name}</Text>
         <Text style={[styles.todoText, { color: "#818993" }]}>{createdAt}</Text>
       </View>
+      <Button style={styles.delete} onPress={() => deleteContact(id)}>
+        <DeleteButton />
+      </Button>
     </View>
   );
 }
